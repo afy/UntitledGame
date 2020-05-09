54570597f651c42b50240f2de1388682f57f244f
hannes f <hannes.furhoff@hotmail.com>
2020-05-09 14:49:52 +0200

    Added GLEW, glm, basic test renderer + shader

--- Source/Client/Client.cpp
+++ Source/Client/Client.cpp
@@ -1,23 +1,159 @@
-#include "SFML/Graphics.hpp"
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include "glew-2.1.0/include/GL/glew.h"
+#include "SFML/Graphics.hpp"
+#include "SFML/OpenGL.hpp"
+#include "glm/glm.hpp"
+#include "glm/ext.hpp"
+
+GLuint LoadShaders(const char * vertex_file_path, const char * fragment_file_path) {
+
+	// Create the shaders
+	GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+	GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+	// Read the Vertex Shader code from the file
+	std::string VertexShaderCode;
+	std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
+	if (VertexShaderStream.is_open()) {
+		std::stringstream sstr;
+		sstr << VertexShaderStream.rdbuf();
+		VertexShaderCode = sstr.str();
+		VertexShaderStream.close();
+	}
+	else {
+		printf("Impossible to open %s. Are you in the right directory ? Don't forget to read the FAQ !\n", vertex_file_path);
+		getchar();
+		return 0;
+	}
+
+	// Read the Fragment Shader code from the file
+	std::string FragmentShaderCode;
+	std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
+	if (FragmentShaderStream.is_open()) {
+		std::stringstream sstr;
+		sstr << FragmentShaderStream.rdbuf();
+		FragmentShaderCode = sstr.str();
+		FragmentShaderStream.close();
+	}
+
+	GLint Result = GL_FALSE;
+	int InfoLogLength;
+
+	// Compile Vertex Shader
+	printf("Compiling shader : %s\n", vertex_file_path);
+	char const * VertexSourcePointer = VertexShaderCode.c_str();
+	glShaderSource(VertexShaderID, 1, &VertexSourcePointer, NULL);
+	glCompileShader(VertexShaderID);
+
+	// Check Vertex Shader
+	glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
+	glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+	if (InfoLogLength > 0) {
+		std::vector<char> VertexShaderErrorMessage(InfoLogLength + 1);
+		glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
+		printf("%s\n", &VertexShaderErrorMessage[0]);
+	}
+
+	// Compile Fragment Shader
+	printf("Compiling shader : %s\n", fragment_file_path);
+	char const * FragmentSourcePointer = FragmentShaderCode.c_str();
+	glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer, NULL);
+	glCompileShader(FragmentShaderID);
+
+	// Check Fragment Shader
+	glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
+	glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+	if (InfoLogLength > 0) {
+		std::vector<char> FragmentShaderErrorMessage(InfoLogLength + 1);
+		glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
+		printf("%s\n", &FragmentShaderErrorMessage[0]);
+	}
+
+	// Link the program
+	printf("Linking program\n");
+	GLuint ProgramID = glCreateProgram();
+	glAttachShader(ProgramID, VertexShaderID);
+	glAttachShader(ProgramID, FragmentShaderID);
+	glLinkProgram(ProgramID);
+
+	// Check the program
+	glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
+	glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+	if (InfoLogLength > 0) {
+		std::vector<char> ProgramErrorMessage(InfoLogLength + 1);
+		glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
+		printf("%s\n", &ProgramErrorMessage[0]);
+	}
+
+	glDetachShader(ProgramID, VertexShaderID);
+	glDetachShader(ProgramID, FragmentShaderID);
+
+	glDeleteShader(VertexShaderID);
+	glDeleteShader(FragmentShaderID);
+
+	return ProgramID;
+}
 
 int main() 
 {
+	//Base setup
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Untitled Game");
+	sf::Event ev;
+	sf::ContextSettings cs = window.getSettings();
+	std::cout << "using OPENGL version: " << cs.majorVersion << "." << cs.minorVersion << "\n";
+	std::cout << "using GLEW version: " << glewGetString(GLEW_VERSION) << "\n";
+	bool running = true;
 
-	while (window.isOpen()) 
+	//GLEW setup
+	if (glewInit() != GLEW_OK) 
 	{
-		sf::Event e;
-		while (window.pollEvent(e)) 
+		return -1;
+	}
+
+	//GL setup
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+	//Shaders
+	GLuint shader1 = LoadShaders("./Shaders/Test.vert", "./Shaders/Test.frag");
+
+	// VAOs and objects 
+	static const GLfloat obj1[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f,  1.0f, 0.0f,
+	};
+
+	GLuint vbuff;
+	glGenBuffers(1, &vbuff);
+	glBindBuffer(GL_ARRAY_BUFFER, vbuff);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(obj1), obj1, GL_STATIC_DRAW);
+
+	// Loop
+	while (running) 
+	{
+		while (window.pollEvent(ev)) 
 		{
-			if (e.type == sf::Event::Closed) 
+			if (ev.type == sf::Event::Closed)
 			{
-				window.close();
+				running = false;
 			}
 		}
 
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glUseProgram(shader1);
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vbuff);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDisableVertexAttribArray(0);
+
 		window.display();
 	}
+
+	window.close();
 
 	return 0;
 }
---
+++ Source/Client/Shaders/Test.vert
@@ -0,0 +1,8 @@
+#version 330 core
+layout(location = 0) in vec3 vpos;
+
+void main() 
+{
+	gl_Position.xyz = vpos;
+	gl_Position.w = 1.0;
+}
